29481420063a9eb323b1cacafd461c1c6b5ea72f
Alexandr Ivanov <Kurotsuchi346@yandex.ru>
2022-05-28 23:54:37 +0300

    Style: add adaptive FavoriteCats & some fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,14 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header__links">
-        <NavLink to="/" className="header__link">Все котики</NavLink>
-        <NavLink to="/liked-cats" className="header__link">Любимые котики</NavLink>
+        <NavLink to="/"
+          className={({isActive}) => (isActive ? "header__link header__link_active" : "header__link")}>
+          Все котики
+        </NavLink>
+        <NavLink to="/liked-cats"
+          className={({isActive}) => (isActive ? "header__link header__link_active" : "header__link")}>
+          Любимые котики
+        </NavLink>
       </div>
     </header>
   );
